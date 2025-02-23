3c717f34284bc42d98146667095010bfa5d9048d
yugalkhanal <yugalkhanal5@gmail.com>
2025-02-23 12:39:21 +0000

    updated

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -5,11 +5,11 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.prettierd,
         null_ls.builtins.formatting.nixpkgs_fmt,
         null_ls.builtins.formatting.clang_format, -- For C/C++ formatting
         null_ls.builtins.formatting.latexindent,
-        null_ls.builtins.formatting.black, -- Python formatter
+        null_ls.builtins.formatting.black,    -- Python formatter
       },
     })
     vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, { desc = "Format the code with LSP" })
