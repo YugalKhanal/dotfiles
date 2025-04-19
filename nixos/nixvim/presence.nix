692b003a8705a52825f386757be92502dfd6c515
yugalkhanal <yugalkhanal5@gmail.com>
2025-04-19 02:00:36 +0100

    fuck presence

--- nixos/nixvim/presence.nix
+++ nixos/nixvim/presence.nix
@@ -1,20 +1,7 @@
 {
-  # programs.nixvim = {
-  #   config = {
-  #     plugins.presence-nvim = {
-  #       enable = true;
-  #
-  #       # settings = {
-  #       # auto-update = true;
-  #       # debounce_timeout = 0;
-  #       # show_time = true;
-  #       # main_image = "language";
-  #       # editing_text = "Editing...";
-  #       # reading_text = "Reading...";
-  #       # terminal_text = "Using Terminal...";
-  #       # workspace_text = "Working on %s";
-  #       # };
-  #     };
-  #   };
-  # };
+  programs.nixvim = {
+    plugins.presence-nvim = {
+      enable = true;
+    };
+  };
 }
